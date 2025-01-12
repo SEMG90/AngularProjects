80063fc102008b8c55b822962073eff959dc3601
SEMG90 <mmagdy1690@gmail.com>
2025-01-12 15:08:58 +0200

    commit v2

--- login_v8/src/app/app.component.ts
+++ login_v8/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [LoginComponent]
 })
 export class AppComponent {
   title = 'login_v8';
